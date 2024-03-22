7fda29b1d640ebfdab3b5e31a10302175e07c7e3
rattytaty <dmolodovskyi@gmail.com>
2024-03-22 23:59:01 +0000

    board page

--- src/components/Boards/BoardPage.tsx
+++ src/components/Boards/BoardPage.tsx
@@ -1,10 +1,19 @@
+import {Box} from '@mui/material';
 import React from 'react';
 import {HeaderBackground} from "./HeaderBackground";
 
 export const BoardPage = () => {
 
     return <>
-<HeaderBackground/>
+        <HeaderBackground/>
+        <Box sx={{
+            background: "#2a3142",
+            borderRadius: 1,
+            p: 1,
+            m: 2
+        }}>
 
-        </>
+        </Box>
+
+    </>
 };
